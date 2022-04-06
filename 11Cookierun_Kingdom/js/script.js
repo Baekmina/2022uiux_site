89203f3045b2da9d647f6c40ebe60753eef62c55
백민아 <koyoa12@naver.com>
2022-04-06 23:19:14 +0900

    full v4

--- 11Cookierun_Kingdom/js/script.js
+++ 11Cookierun_Kingdom/js/script.js
@@ -1,34 +1,40 @@
 // fullpage
 $("#fullpage").fullpage({
-    menu:".topMenu",
-    anchors:["p_one","p_two","p_three","p_four","p_five"],
-    responsiveWidth:1000,
+  menu: ".topMenu",
+  anchors: ["p_one", "p_two", "p_three", "p_four", "p_five"],
+  responsiveWidth: 1000,
 })
 
 // modal
 $(".youtube").colorbox({
-    iframe:true,
-    innerWidth:1000,
-    innerHeight:562.5,
+  iframe: true,
+  innerWidth: 1000,
+  innerHeight: 562.5,
 });
 
 // swiper
 const swiper = new Swiper('.swiper', {
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+  loop: true,
+  autoplay: { delay: 2000, },
+
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
+
+  // swiper tabmenu
+$("#tabmenu li").click(function () {
+  let i = $(this).index();
+  $(".swiper-slide img").hide();
+  $(".swiper-slide img").eq(i).show();
+})
