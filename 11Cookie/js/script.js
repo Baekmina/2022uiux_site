cb7d7f1272b41abec82930f5320be6af149f4f42
백민아 <koyoa12@naver.com>
2022-04-07 18:28:02 +0900

    full v6

--- 11Cookie/js/script.js
+++ 11Cookie/js/script.js
@@ -13,10 +13,17 @@ $(".youtube").colorbox({
 });
 
 // swiper
-const swiper = new Swiper('.swiper', {
-  loop: true,
-  autoplay: { delay: 2000, },
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  slidesPerView: 5,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
 
+var swiper1 = new Swiper('.mainSwiper', {
+  loop: true,
+  spaceBetween: 10,
+  autoplay: { delay: 2000, },
 
   pagination: {
     el: '.swiper-pagination',
@@ -27,14 +34,39 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
+  thumbs: {
+    swiper: swiper,
+  },
 });
 
-  // swiper tabmenu
-$("#tabmenu li").click(function () {
-  let i = $(this).index();
-  $(".swiper-slide img").hide();
-  $(".swiper-slide img").eq(i).show();
+
+// page3 swiper
+
+// $(".tab_wrap ul li").click(function(){
+
+//   let i = $(".tab_con .tab_list").index();
+
+//   $("tab_con .tab_list").hide();
+//   $(this).eq(i).show();
+// })
+
+$(".face li").click(function(){
+  $(".face li").removeClass("active")
+  $(this).addClass("active")
 })
+
+
+var swiper2_1_menu = new Swiper(".swiper2_1_menu",{
+  freeMode: true,
+  watchSlidesProgress: true,
+})
+
+var swiper2_1_slide = new Swiper(".swiper2_1_slide",{
+  loop:true,
+  slidesPerView: "auto",
+
+  thumbs: {
+    swiper: swiper2_1_menu,
+  },
+})
+
