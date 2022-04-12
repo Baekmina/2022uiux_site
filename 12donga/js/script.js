732f1b09a40da4ffa3684ef583ddacdd03711643
백민아 <koyoa12@naver.com>
2022-04-12 18:22:33 +0900

    donga

---
+++ 12donga/js/script.js
@@ -0,0 +1,17 @@
+// swiper slide
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
